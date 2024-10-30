2000a8f006f651400a9e192a02d0b3b1d635417c
kl4nv <kupull94@bk.ru>
2024-10-30 16:15:19 +1000

    Add files via upload

---
+++ 13_2.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+dp = Dispatcher(Bot(token='my_api'), storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
